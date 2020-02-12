e056336c42e4aa0487e8ebe07f9250d910db878a
Alexander Weickmann <alexander.weickmann@link-time.com>
2020-02-12 12:21:37 +0100

    convert setup-idea.sh to bash script and set proper unix permissions

--- src/main/resources/archetype-resources/setup-idea.sh
+++ src/main/resources/archetype-resources/setup-idea.sh
@@ -1,27 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Determine the location of the script
-# resolve symbolic links
-PRG="${0}"
-while [ -h "${PRG}" ]; do
-  ls=$(ls -ld "${PRG}")
-  link=$(expr "${ls}" : '.*-> \(.*\)$')
-  if expr "${link}" : '/.*' >/dev/null; then
-    PRG="${link}"
-  else
-    PRG=$(dirname "${PRG}")/"${link}"
-  fi
-done
-SCRIPT_DIR=$(dirname "${PRG}")
-
-# ------------------------------------------------------------------------------------------------------------
 # Register prepared run configurations with IntelliJ Project configuration
 # Note: These are not checked into the repository under .idea folder, as users
 #       often delete these when opening a project for the first time in Idea
-# ------------------------------------------------------------------------------------------------------------
-mkdir -p "$SCRIPT_DIR/.idea/runConfigurations"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p .idea/runConfigurations
 for f in "$SCRIPT_DIR"/runConfigurations/*; do
   echo "symlink $f"
   filename=$(basename -- "$f")
-  ln -sfn "$f" "$SCRIPT_DIR/.idea/runConfigurations/$filename"
+  ln -sfn "$f" ".idea/runConfigurations/$filename"
 done
