130bcea0d6e71e39d532a1d875fb1be184d4e8d0
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2020-10-29 13:25:29 +0100

    update pom.xml and runConfigurations

--- src/main/resources/archetype-resources/setup-idea.sh
+++ src/main/resources/archetype-resources/setup-idea.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Determine the location of the script
+SCRIPT_DIR="$(
+  cd "$(dirname "$0")" || exit
+  pwd -P
+)"
+
+# ------------------------------------------------------------------------------------------------------------
 # Register prepared run configurations with IntelliJ Project configuration
 # Note: These are not checked into the repository under .idea folder, as users
 #       often delete these when opening a project for the first time in Idea
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-mkdir -p .idea/runConfigurations
+# ------------------------------------------------------------------------------------------------------------
+mkdir -p "$SCRIPT_DIR/.idea/runConfigurations"
 for f in "$SCRIPT_DIR"/runConfigurations/*; do
   echo "symlink $f"
   filename=$(basename -- "$f")
-  ln -sfn "$f" ".idea/runConfigurations/$filename"
+  ln -sfn "$f" "$SCRIPT_DIR/.idea/runConfigurations/$filename"
 done
