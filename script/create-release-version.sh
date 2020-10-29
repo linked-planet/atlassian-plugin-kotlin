8e3b9bf3fb5f67548feab1c18a31921763912f76
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2020-10-29 13:30:02 +0100

    simplify release script

--- script/create-release-version.sh
+++ script/create-release-version.sh
@@ -3,18 +3,21 @@
 set -e
 
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <version>"
+    echo "Usage: $0 version"
     exit 1
 fi
 
 VERSION=$1
 
-get_version_from_maven() {
-    mvn help:evaluate -Dexpression=project.version -q -DforceStdout | tail -n 1
-}
-
-mvn build-helper:parse-version versions:set "-DnewVersion=${VERSION}" versions:commit
+# set non-snapshot version number as specified in parameter
+mvn versions:set "-DnewVersion=${VERSION}" versions:commit
 git add pom.xml && git commit -m "release v${VERSION}" && git tag "v${VERSION}" && git push && git push --tags
-mvn build-helper:parse-version versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT" versions:commit
-next_version=$(get_version_from_maven)
-git add pom.xml && git commit -m "[skip ci] set development version ${next_version}" && git push
+
+# prepare for next snapshot version
+MAJOR_VERSION=$(echo "$VERSION" | cut -d . -f1)
+MINOR_VERSION=$(echo "$VERSION" | cut -d . -f2)
+INCREMENT_VERSION=$(echo "$VERSION" | cut -d . -f3)
+NEXT_INCREMENT_VERSION=$((INCREMENT_VERSION + 1))
+NEXT_SNAPSHOT_VERSION="$MAJOR_VERSION.$MINOR_VERSION.$NEXT_INCREMENT_VERSION-SNAPSHOT"
+mvn versions:set "-DnewVersion=${NEXT_SNAPSHOT_VERSION}" versions:commit
+git add pom.xml && git commit -m "[skip ci] set development version $NEXT_SNAPSHOT_VERSION" && git push
