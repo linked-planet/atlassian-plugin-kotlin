ec4d7aaf4c94c0b8136a9e1134723fe2de05e958
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2020-12-21 12:32:26 +0100

    release.sh - only update package___deploy.xml if the file exists

--- src/main/resources/archetype-resources/pipelines/release.sh
+++ src/main/resources/archetype-resources/pipelines/release.sh
@@ -13,7 +13,9 @@ set_commit_version() {
   V=$1
   COMMIT_MSG=$2
   mvn -B versions:set "-DnewVersion=$V" versions:commit
-  sed -i -E "s/(.*)-[0-9]+\.[0-9]+\.[0-9]+.*\.jar/\1-$V.jar/g" runConfigurations/package___deploy.xml
+  if test -f "runConfigurations/package___deploy.xml"; then
+    sed -i -E "s/(.*)-[0-9]+\.[0-9]+\.[0-9]+.*\.jar/\1-$V.jar/g" runConfigurations/package___deploy.xml
+  fi
   git ls-files . | grep 'pom.xml' | xargs git add &&
     git ls-files . | grep 'package___deploy.xml' | xargs git add &&
     git commit -m "$COMMIT_MSG"
