e78f0e42cdb3148505d6a61ccefba1edf4a34e7b
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2021-03-15 10:34:59 +0100

    release v3.1.3

--- src/main/resources/archetype-resources/regenerate.sh
+++ src/main/resources/archetype-resources/regenerate.sh
@@ -32,7 +32,7 @@ echo
 mvn archetype:generate -B -U \
     "-DarchetypeGroupId=com.linked-planet.maven.archetype" \
     "-DarchetypeArtifactId=atlassian-plugin-kotlin" \
-    "-DarchetypeVersion=3.1.3-SNAPSHOT" \
+    "-DarchetypeVersion=3.1.3" \
     "-DatlassianApp=$atlassianApp" \
     "-DgroupId=$groupId" \
     "-DartifactId=$artifactId" \
